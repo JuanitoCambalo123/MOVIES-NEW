72296e81b280ed69ae9ddedb92479df70de02789
JuanitoCambalo123 <145818653+JuanitoCambalo123@users.noreply.github.com>
2023-10-14 12:45:26 +0800

    yes
    yes

--- BSIT3L-Movies/Controllers/HomeController.cs
+++ BSIT3L-Movies/Controllers/HomeController.cs
@@ -3,38 +3,48 @@ using Microsoft.AspNetCore.Mvc;
 using BSIT3L_Movies.Models;
 using System.Collections.Generic;
 
-namespace BSIT3L_Movies.Controllers;
-
-public class HomeController : Controller
+namespace BSIT3L_Movies.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly List<MovieViewModel> _movies;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-        _movies = new List<MovieViewModel>
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<MovieViewModel> _movies;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
-            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
-            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
-        };
-    }
+            _logger = logger;
+            _movies = new List<MovieViewModel>
+            {
 
-    public IActionResult Index()
-    {
-        return View(_movies);
-    }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
+new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
+new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+new MovieViewModel { Id = 4, Name = "John Wick: Chapter 4", Rating = "4", ReleaseYear = 2023, Genre = "Action" },
+new MovieViewModel { Id = 5, Name = "The Avengers", Rating = "4", ReleaseYear = 2012, Genre = "Action/Sci-Fi" },
+new MovieViewModel { Id = 6, Name = "Avatar", Rating = "4", ReleaseYear = 2009, Genre = "Science Fiction/Action" },
+new MovieViewModel { Id = 7, Name = "Interstellar", Rating = "4", ReleaseYear = 2014, Genre = "Science Fiction/Drama" },
+new MovieViewModel { Id = 8, Name = "The Matrix", Rating = "4", ReleaseYear = 1999, Genre = "Science Fiction/Action" },
+new MovieViewModel { Id = 9, Name = "The Godfather", Rating = "5", ReleaseYear = 1972, Genre = "Crime/Drama" },
+new MovieViewModel { Id = 10, Name = "Pulp Fiction", Rating = "4", ReleaseYear = 1994, Genre = "Crime/Drama" },
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
-
--- BSIT3L-Movies/Controllers/MoviesController.cs
+++ BSIT3L-Movies/Controllers/MoviesController.cs
@@ -1,36 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using BSIT3L_Movies.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BSIT3L_Movies.Controllers
 {
-	public class MoviesController : Controller
+    public class MoviesController : Controller
     {
         private List<MovieViewModel> _movies;
+
         public MoviesController()
-		{
-            // Sample movie data
+        {
             _movies = new List<MovieViewModel>
             {
-               new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
-            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
-            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
+new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
+new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+new MovieViewModel { Id = 4, Name = "John Wick: Chapter 4", Rating = "4", ReleaseYear = 2023, Genre = "Action" },
+new MovieViewModel { Id = 5, Name = "The Lion King", Rating = "5", ReleaseYear = 1994, Genre = "Animation/Adventure" },
+new MovieViewModel { Id = 6, Name = "Toy Story 4", Rating = "4", ReleaseYear = 2019, Genre = "Animation/Comedy" },
+new MovieViewModel { Id = 7, Name = "The Dark Knight", Rating = "5", ReleaseYear = 2008, Genre = "Action/Crime" },
+new MovieViewModel { Id = 8, Name = "Avengers: Endgame", Rating = "4", ReleaseYear = 2019, Genre = "Action/Adventure" },
+new MovieViewModel { Id = 9, Name = "Forrest Gump", Rating = "5", ReleaseYear = 1994, Genre = "Drama/Romance" },
+new MovieViewModel { Id = 10, Name = "The Matrix", Rating = "4", ReleaseYear = 1999, Genre = "Science Fiction/Action" },
+
             };
         }
+
         public ActionResult Random()
         {
-            var movie = new MovieViewModel() { Name = "Avatar", Rating = "5" };
+            var movie = new MovieViewModel() { Name = "Avatar", Rating = "4", ReleaseYear = 2009, Genre = "Science Fiction/Action" };
             return View(movie);
         }
+
         public ActionResult GetMovie(int id)
         {
             var movie = _movies.Find(m => m.Id == id);
             if (movie == null)
                 return NotFound();
             return View(movie);
-
         }
-
     }
 }
-
